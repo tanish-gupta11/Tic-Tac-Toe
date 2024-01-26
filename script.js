fbd92601af53e820d56cdc5f61e0685dcc3965d1
Tanish Gupta <63304954+tanish-gupta11@users.noreply.github.com>
2024-01-26 23:29:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+// Winning pattern of tic tac toe 3 horizontal, 3 Vertical, 2 diagonal
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turn0 = true;   //player x, player 0
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+const resetGame = () => {
+    turn0 = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
+
+boxes.forEach((box) => {
+    box.addEventListener("click",() => {
+        if(turn0){
+            //player0
+            box.innerText = "0";
+            box.style.color = "blue";
+            turn0 = false;
+        } else {
+             //playerx
+             box.innerText = "X";
+             box.style.color = "#b0413e";
+             turn0 = true;
+        }
+        box.disabled = true;
+        checkWinner();
+    });
+});
+
+const disableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled = true;
+    }
+}
+const enableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+
+
+const checkWinner = () => {
+    for(let pattern of winPatterns){
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if (pos1val != "" && pos2val != "" && pos3val != ""){
+            if(pos1val === pos2val && pos2val === pos3val){
+                console.log("Winner", pos1val);
+                showWinner(pos1val);
+            }
+        }
+    }
+}
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
